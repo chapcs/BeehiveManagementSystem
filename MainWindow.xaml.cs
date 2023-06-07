526773efc369eeac0906765284a38c5c94a48747
chapcs <benchapman003@gmail.com>
2023-06-07 08:53:54 -0400

    Added HoneyVault static class

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,4 +25,62 @@ namespace BeehiveManagementSystem
             InitializeComponent();
         }
     }
+
+    public static class HoneyVault
+    {
+        const float NECTAR_CONVERSION_RATIO = .19f;
+        const float LOW_LEVEL_WARNING = 10f;
+
+        private static float honey = 25f;
+        private static float nectar = 100f;
+
+        static string StatusReport
+        {
+            get
+            {
+                string warning = "";
+                if (honey < LOW_LEVEL_WARNING)
+                    warning = "LOW HONEY - ADD MANUFACTURER\n";
+                else if (nectar < LOW_LEVEL_WARNING)
+                    warning += "LOW NECTAR - ADD COLLECTOR\n";
+                return $"Honey: {honey}\nNectar: {nectar}\n" + warning;
+            }
+        }
+
+        public static void ConvertNectarToHoney(float amount)
+        {
+            if (amount <= nectar)
+                amount = nectar;
+            else
+                nectar -= amount;
+            honey += (amount * NECTAR_CONVERSION_RATIO);
+        }
+
+        public static bool ConsumeHoney(float amount)
+        {
+            if (amount >= honey)
+            {
+                honey -= amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void CollectNectar(float amount)
+        {
+            if (amount > 0)
+                nectar += amount;
+        }
+    }
+
+    public class Bee
+    {
+        public Bee()
+        {
+
+        }
+    }
 }
